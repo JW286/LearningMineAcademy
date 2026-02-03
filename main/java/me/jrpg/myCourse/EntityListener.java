d0c38b5a7e57d3fb9b0d036ba1ef0dcebe7654e7
JW286 <joshwilliams286@hotmail.com>
2026-02-03 14:34:41 -0700

    Add files via upload

--- main/java/me/jrpg/myCourse/EntityListener.java
+++ main/java/me/jrpg/myCourse/EntityListener.java
@@ -4,23 +4,29 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
-    /*
-    * EntityListener is required for an EventHandler that involves entity interactions
-    */
+/*
+* EntityListener is required for an EventHandler that involves entity interactions
+*/
 
 public class EntityListener implements Listener {
+
     /**
-     *
      * @param event Represents the entity that you are interacting with
      */
     @EventHandler
-    public void onMobRightClick(PlayerInteractEntityEvent event){
-        if (event.getRightClicked().getType() == EntityType.CHICKEN)
+    public void onMobRightClick(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked().getType() == EntityType.CHICKEN && event.getHand().equals(EquipmentSlot.OFF_HAND)) {
             //If a player interacts with a chicken this will occur
-            if (!event.getRightClicked().isGlowing())
+            if (!event.getRightClicked().isGlowing()) {
                 event.getRightClicked().setGlowing(true);
-        if (event.getRightClicked().getType() == EntityType.COW)
-            event.getRightClicked().setFireTicks(9999);
+                event.getPlayer().sendMessage("Glowing");
+            }
+            else {
+                event.getRightClicked().setGlowing(false);
+                event.getPlayer().sendMessage("Not Glowing");
+            }
+        }
     }
 }
